c4d011b201e6da8155153d459f1f1879afd75bce
96057 <gh0stboy2052@gmail.com>
2024-06-24 20:34:03 -0400

    Add files via upload

---
+++ x.py
@@ -0,0 +1,72 @@
+#!/usr/lib/python3
+
+import os
+import argparse
+import colorama
+from colorama import Fore, Back, Style
+
+def write_username_once():
+    # Define the flag file and user file paths
+    flag = os.path.expanduser("~/x/xs/flag.flag")
+    user_file = os.path.expanduser("~/.x/xs/user.txt")
+
+    # Check if the flag file exists
+    if not os.path.exists(flag):
+        # If the flag file doesn't exist, prompt for the username
+        print("username of os")
+        user = input("username: ")
+
+        # Write the username to the user file
+        with open(user_file, 'a') as f:
+            f.write(user + "\n")
+
+        # Create the flag file to indicate that the code has run once
+        with open(flag, 'w') as f:
+            f.write("This code has already run.")
+
+    else:
+        # If the flag file exists, read the username from the user file
+        with open(user_file, 'r') as f:
+            user = f.readline().strip()
+
+    # Return the username for use in other parts of the script
+    return user
+
+def handle_su():
+    # Handle the su subcommand
+    new_user = input(Fore.BLUE + "new username: ")
+    pwd = os.path.expanduser("~/.x/xs/user.txt")
+
+    # Remove the existing user file
+    if os.path.exists(pwd):
+        os.remove(pwd)
+
+    # Write the new username to the user file
+    with open(pwd, 'w') as f:
+        f.write(new_user + "\n")
+
+    print(Fore.Blue + f"user changed to '{new_user}'")
+
+def main():
+    try:
+        # Create the top-level parser
+        parser = argparse.ArgumentParser(description='Switch User For X')
+        parser.add_argument('-su', action='store_true', help='Switch User')
+
+        # Parse the arguments
+        args = parser.parse_args()
+
+        if args.su:
+            handle_su()
+        else:
+            # Default action if no subcommand is provided
+            user = write_username_once()
+            # Execute other commands using the username
+            os.system("rm zsh_history")
+            os.system("clear")
+            os.system("pkill -KILL -u " + user)
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by user. Exiting...")
+
+if __name__ == "__main__":
+    main()
---
+++ xsetup.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import os
+
+ans = input("Do you wish do update and upgrade Your OS? y/n ")
+if ans == 'y':
+        os.system("sudo apt update")
+        os.system("sudo apt upgrade -y")
+os.system("sudo apt install python3 -y")
+os.system("sudo apt install python3-pip -y")
+os.system("pip install -r requirements.txt")
+os.system("sudo chmod +x x")
+os.system("sudo chmod 700 x")
+os.system("sudo chmod 755 x")
+os.system("sudo cp x /bin/")
+os.system("clear")
+print("""
+
+  ####################################
+ ######################################
+#####           #####          #########
+######           ###          ##########
+#######          ##          ###########
+########        ##          ############
+#########      ##          #############
+##########    ##          ##############
+###########  ##          ###############
+########### #          #################
+############          ##################
+###########          ##    #############
+##########          ##      ############
+#########          ##        ###########
+########          ##          ##########
+#######          ####          #########
+######          ######          ########
+#####          ########           ######
+ ######################################
+   ###################################
+CREATED BY: 96057
+
+An Emergancy command to delete the Shell History and Logout.
+
+REMEMBER!:
+Control and Privacy is an Illusion
+
+Type x and ENTER
+""")
+os.system("x -h")
